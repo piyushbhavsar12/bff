6418ad22b0b9c222e13bfa936c44922b85745a40
Amruth-Vamshi <amruth.vamshi@esmagico.in>
2023-04-24 13:31:42 +0530

    Added reactions to query.

---
+++ prisma/migrations/20230424073444_add_id_and_reaction_to_query/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - The required column `id` was added to the `query` table with a prisma-level default value. This is not possible if the table is not empty. Please add this column as optional, then populate it before making it required.
+
+*/
+-- AlterTable
+ALTER TABLE "query" ADD COLUMN     "id" UUID NOT NULL,
+ADD COLUMN     "reaction" SMALLINT NOT NULL DEFAULT 0;
