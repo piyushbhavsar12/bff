58f2150e03cae4207acc0b60491f3bd42f538a3d
Amruth-Vamshi <amruth.vamshi@esmagico.in>
2023-07-10 19:13:42 +0530

    Added OTP on aadhar.

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -83,6 +83,8 @@ export class UserController {
       return this.userService.sendOTP(identifier,"Mobile")
     } else if(identifier.length==14 && /^[6-9]\d{9}$/.test(identifier.substring(0,10))){
       return this.userService.sendOTP(identifier,"MobileAadhar")
+    } else if(identifier.length==12 && /^\d+$/.test(identifier)){
+      return this.userService.sendOTP(identifier,"Aadhar")
     } else {
       return {
         "status": "NOT_OK",
@@ -97,6 +99,8 @@ export class UserController {
       return this.userService.verifyOTP(body.identifier,body.otp,"Mobile")
     } else if(body.identifier.length==14 && /^[6-9]\d{9}$/.test(body.identifier.substring(0,10))){
       return this.userService.verifyOTP(body.identifier,body.otp,"MobileAadhar")
+    } else if(body.identifier.length==12 && /^\d+$/.test(body.identifier)){
+      return this.userService.sendOTP(body.identifier,"Aadhar")
     } else {
       return {
         "status": "NOT_OK",
@@ -157,6 +161,8 @@ export class UserController {
       return this.userService.getUserData(identifier,"Mobile")
     } else if(identifier.length==14 && /^[6-9]\d{9}$/.test(identifier.substring(0,10))){
       return this.userService.getUserData(identifier,"MobileAadhar")
+    } else if(identifier.length==12 && /^\d+$/.test(identifier)){
+      return this.userService.sendOTP(identifier,"Aadhar")
     } else {
       return {
         "status": "NOT_OK",
