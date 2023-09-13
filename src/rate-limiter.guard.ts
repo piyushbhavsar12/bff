21ce13852d12a4931f382785b7aef13fc3b2783f
Amruth-Vamshi <amruth.vamshi@esmagico.in>
2023-09-13 15:11:02 +0530

    updated rate limiter gaurd

--- src/rate-limiter.guard.ts
+++ src/rate-limiter.guard.ts
@@ -1,11 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
 import { ThrottlerGuard } from '@nestjs/throttler';
 import { Request } from 'express';
 
 @Injectable()
 export class RateLimiterGuard extends ThrottlerGuard {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    console.log("canActivate")
+    console.log(request.context.config.url)
+
+    // Check if the request path is "/metrics"
+    if (request.context.config.url === '/metrics') {
+      return true; // Bypass rate limiting for "/metrics"
+    }
+
+    // For other routes, apply rate limiting
+    return super.canActivate(context);
+  }
+
   async getKey(request: Request): Promise<string> {
     // In this example, we're using the IP address as the key for rate limiting.
     return request.ip;
   }
 }
+
