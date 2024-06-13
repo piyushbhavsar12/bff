56162778d53ddcfd90ac820d81d03a58c61e7daa
Yash Mittal <mittal.yash12@hotmail.com>
2024-06-13 17:50:19 +0530

    fix: guards on decryption

--- src/xstate/prompt/prompt.gaurds.ts
+++ src/xstate/prompt/prompt.gaurds.ts
@@ -6,12 +6,20 @@ export const promptGuards = {
   ifMultipleAadhaar: (_, event) =>
     event.data == "This mobile number taged with multiple records.",
 
-  ifInValidScheme: (_, event) =>
-    !VALID_SCHEMES.includes(event.data.prompt.input.schemeName),
+  //   ifInValidScheme: (_, event) =>
+  //     !VALID_SCHEMES.includes(event.data.prompt.input.schemeName),
 
-  ifNoRecordsFound: (context, event) =>
-    event.data ==
-    `No Record Found for this (${context.userAadhaarNumber}) Aadhar/Ben_id/Mobile.`,
+  ifNoRecordsFound: (context, event) => {
+    const pattern =
+      /No Record Found for this \((.*?)\) Aadhar\/Ben_id\/Mobile\./;
+    return (
+      pattern.test(event.data) ||
+      ((event.data as string).startsWith("No Record Found for this") &&
+        (event.data as string).endsWith("Aadhar/Ben_id/Mobile."))
+    );
+  },
+  // event.data ==
+  //   `No Record Found for this (${context.userAadhaarNumber}) Aadhar/Ben_id/Mobile.`,
 
   ifOTPSend: (_, event) => event.data == "OTP send successfully!",
 
