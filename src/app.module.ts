91c316ec04284b4cb9463694072a7f5796e6d42c
piyushbhavsar12 <piyush.bhavsar@tekditechnologies.com>
2025-02-19 16:30:34 +0530

    upd

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,6 @@ import { PrismaService } from "./global-services/prisma.service";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import { UserModule } from "./modules/user/user.module";
 import { APP_GUARD, APP_PIPE } from "@nestjs/core";
-import { CustomLogger } from "./common/logger";
 import { ConversationService } from "./modules/conversation/conversation.service";
 import { ConversationModule } from "./modules/conversation/conversation.module";
 import { PrometheusModule } from "@willsoto/nestjs-prometheus";
@@ -13,8 +12,6 @@ import { RateLimiterGuard } from './rate-limiter.guard';
 import { ThrottlerModule } from '@nestjs/throttler';
 import { MonitoringModule } from "./modules/monitoring/monitoring.module";
 import { PromptModule } from "./xstate/prompt/prompt.module";
-import { TelemetryModule } from "./modules/telemetry/telemetry.module";
-import { TelemetryService } from "./modules/telemetry/telemetry.service";
 import { MonitoringController } from "./modules/monitoring/monitoring.controller";
 import { CacheProvider } from "./modules/cache/cache.provider";
 import { HttpModule } from "@nestjs/axios";
@@ -25,7 +22,35 @@ import { MetricsModule } from './metrics/metrics.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        name: 'Telemetry',
+        transport: {
+          targets: [
+            {
+              target: 'pino-pretty'
+            },
+            {
+              level: process.env.NODE_ENV !== 'production' ? 'debug' : 'warn',
+              target: 'pino-loki',
+              options: {
+                batching: true,
+                interval: 5,
+                host: process.env.LOKI_INTERNAL_BASE_URL,
+                labels: {
+                  app: 'Telemetry',
+                  namespace: process.env.NODE_ENV || 'development',
+                },
+              },
+            }
+          ]
+        }
+      },
+    }),
+    HttpModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     UserModule,
     ConversationModule,
     MonitoringModule,
@@ -35,7 +60,6 @@ import { MetricsModule } from './metrics/metrics.module';
       }
     }),
     PromptModule,
-    TelemetryModule,
     ThrottlerModule.forRoot({
       ttl: 60,
       limit: 10,
@@ -50,17 +74,17 @@ import { MetricsModule } from './metrics/metrics.module';
     PrismaService,
     ConfigService,
     ConversationService,
-    TelemetryService,
     MonitoringController,
     {
       provide: APP_PIPE,
       useClass: ValidationPipe,
     },
-    CustomLogger,
     {
       provide: APP_GUARD,
       useClass: RateLimiterGuard,
     },
+    CacheProvider
   ],
+  exports: [CacheProvider],
 })
 export class AppModule {}
